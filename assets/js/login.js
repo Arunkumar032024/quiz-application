ce359a1b8c28928a84b5fc0f3d36ec14bb5f92e8
arun032024 <arun@cloudshope.com>
2024-12-21 22:13:51 +0530

    add login feature Signed-off-by: arun032024 <arun@cloudshope.com>

---
+++ assets/js/login.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// target element
+const startBtn = document.getElementsByClassName('start-btn')[0];
+const bannerContentBox = document.getElementsByClassName('banner-content')[0];
+const loginContentBox = document.getElementsByClassName('login-content')[0];
+const bannerSectionContentBox = document.getElementsByClassName('banner-section')[0]
+const errorMsg = document.getElementsByClassName('error')[0];
+const inputNameBox = document.getElementsByClassName('input-name')[0];
+startBtn.disabled = true;
+
+loginBtn.addEventListener('click', function(e){
+    bannerContentBox.classList.add('hidden');
+    loginContentBox.classList.remove('hidden');
+    loginBtn.disabled = true;
+    startBtn.disabled = false;
+    startBtn.innerHTML = 'Get Start'
+});
+
+startBtn.addEventListener('click', function(e){
+    if (inputNameBox.value !== ""){
+        bannerSectionContentBox.classList.add('hidden');
+        loginBtn.classList.add('hidden');
+        profileTab.classList.remove('hidden');
+        localStorage.setItem('profileName', inputNameBox.value)
+        profileName = inputNameBox.value;
+        location.reload();
+    } else {
+        errorMsg.classList.remove('hidden');
+    }
+});
+
+inputNameBox.addEventListener('keydown', function(e){
+    errorMsg.classList.add('hidden');
+})
--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,14 +1,15 @@
 "use strict";
 
 // target element 
-const loginBtn = document.getElementsByClassName('login')[0];
+const loginBtn = document.getElementsByClassName('login-btn')[0];
 const profileTab = document.getElementsByClassName('profile')[0];
 
 
-let profileName = "";
+let profileName = localStorage.getItem('profileName');
+console.log(profileName);
 
 document.addEventListener('DOMContentLoaded', (e) => {
-    if (profileName !== ""){
+    if (profileName !== "" && profileName != null){
         loginBtn.classList.add('hidden');
         profileTab.classList.remove('hidden');
         profileTab.innerHTML = profileName;
